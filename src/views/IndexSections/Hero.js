c8c2a81bd4757809aba678ad40ddba532ec26501
ibtissem <rezig.ibtissam@gmail.com>
2021-09-16 15:30:45 +0200

    first commit

--- src/views/IndexSections/Hero.js
+++ src/views/IndexSections/Hero.js
@@ -1,23 +1,4 @@
-/*!
-
-=========================================================
-* Argon Design System React - v1.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-design-system-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
-
-// reactstrap components
 import { Button, Container, Row, Col } from "reactstrap";
 
 class Hero extends React.Component {
@@ -51,8 +32,7 @@ class Hero extends React.Component {
                       style={{ width: "200px" }}
                     />
                     <p className="lead text-white">
-                      A beautiful Design System for Bootstrap 4. It's Free and
-                      Open Source.
+                      Welcome to the crypto world 
                     </p>
                     <div className="btn-wrapper mt-5">
                       <Button
--- src/views/examples/Landing.js
+++ src/views/examples/Landing.js
@@ -1,24 +1,5 @@
-/*!
-
-=========================================================
-* Argon Design System React - v1.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-design-system-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
-// nodejs library that concatenates classes
 import classnames from "classnames";
-
 // reactstrap components
 import {
   Badge,
@@ -74,7 +55,7 @@ class Landing extends React.Component {
                   <Row>
                     <Col lg="6">
                       <h1 className="display-3 text-white">
-                        A beautiful Design System{" "}
+                        A Fucking Design System{" "}
                         <span>completed with examples</span>
                       </h1>
                       <p className="lead text-white">
